681ff23fc821d55592ba773f498a36557826b6c6
yagmurakay <yagmurakay12@gmail.com>
2022-03-01 17:23:09 +0300

    Django Blog App

---
+++ blog/urls.py
@@ -0,0 +1,19 @@
+
+from xml.etree.ElementInclude import include
+from django.contrib import admin
+from django.urls import path,include
+from article import views
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.index, name = "index"),
+    path('about/',views.about, name = "about"),
+    path('articles/',include("article.urls")),
+    path('user/',include("user.urls")),
+
+    
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+  
